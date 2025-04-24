d34251fcafafe4c118bbd068c3b1b1076f4d04f1
cassi35 <cgrsobral@gmail.com>
2025-04-23 21:13:44 -0300

    fiz conexao com meu mysql local

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDbMysql from './lib/db.js'
 
 dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
 
@@ -7,7 +8,12 @@ const app = express()
 const PORT = 4000
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+//connecao mysql  
+connectDbMysql()
+
+//endpoints 
 app.listen(PORT, () => {
     console.log(`servidor rodando na porta: ${PORT}`)
 })
---
+++ backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2'
+const connectDbMysql = ()=>{
+    const conexao = mysql.createConnection({
+        host:"localhost",
+        user:'root',
+        password:"root",
+        database:"adoroFilmes"
+    })
+   conexao.connect((err) => {
+    if(err){
+        throw err 
+    }
+    console.log(`connecao feita com sucesso`)
+   })
+}
+export default connectDbMysql
